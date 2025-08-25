badd1a10b854e97c94da6dd4a85d4f552c9f237e
Abu Bakar <abubakar.akash17@gmail.com>
2025-08-25 21:09:36 +0600

    new css file for priroty and status color options

--- client/taskwave/src/App.jsx
+++ client/taskwave/src/App.jsx
@@ -6,12 +6,15 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import PrivateLayout from "./layouts/PrivateLayout";
 import PublicLayout from "./layouts/PublicLayout";
-import TaskDetails from "./pages/TaskDetails";
+import TaskDetail from "./pages/TaskDetail";
 import NewTask from "./pages/NewTask";
 import ProjectDetails from "./pages/ProjectDetails";
+import NewProject from "./pages/NewProject";
+import ProjectTasksPage from "./components/project/Project Detail/ProjectTasksPage";
+import Team from "./pages/Team";
+import Settings from "./pages/Settings";
 
 function App() {
-
   return (
     <Routes>
       <Route element={<PublicLayout />}>
@@ -23,9 +26,13 @@ function App() {
         <Route path="/" element={<Tasks />} />
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/new-task" element={<NewTask />} />
-        <Route path="/tasks/:id" element={<TaskDetails />} />
+        <Route path="/tasks/:id" element={<TaskDetail />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/new-project" element={<NewProject />} />
         <Route path="/project/:id" element={<ProjectDetails />} />
+        <Route path="/project/:id/tasks" element={<ProjectTasksPage />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/settings" element={<Settings />} />
       </Route>
     </Routes>
   );
